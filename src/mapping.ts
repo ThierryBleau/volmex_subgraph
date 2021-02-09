fc29258f4a00b2688caafa8c5a0560aac92b8585
ThierryBleau <thierry.bleau@protonmail.com>
2021-02-09 17:45:50 -0500

    ethv creation

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,22 +4,19 @@ import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
 
 const BigIntOne =  BigInt.fromI32(1)
 const BigIntZero =  BigInt.fromI32(0)
-
-function isIndex(address: string): boolean {
-    let oToken = ETHV.load(address)
-    return oToken != null
-}
+const ethvAddr = '0xe10bF493577B11f2161b7505b6a707186BFB6208'
 
 
 export function handlePublish(event: Publish): void {
-    let index = VolmexIndex.bind(event.address)
-    let indexAddr = index.tokenAddress().toHexString().toString()
-
-    if (!isIndex(indexAddr)) {
-        return
+    let ethv = ETHV.load(ethvAddr)
+    if (ethv === null) {
+        ethv = new ETHV()
+        ethv.latestUpdate = null
+        ethv.updates = []
+        ethv.numUpdates = BigIntZero
+        return ethv
     }
 
-    let ethv = ETHV.load(indexAddr)
     let update = new ethvUpdate(event.params.id.toHex());
 
     update.blocknumber = event.block.number;
@@ -33,6 +30,4 @@ export function handlePublish(event: Publish): void {
     ethv.updates = updates
     ethv.latestUpdate = update.id
     ethv.save()
-
-
 }

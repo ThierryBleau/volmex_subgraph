4682d52a3e96ca8d2ccff9a69fc4f8a68b286230
ThierryBleau <thierry.bleau@protonmail.com>
2021-02-10 17:38:28 -0500

    deployed subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,6 @@
-import { Publish } from '../generated/ETHV'
-import { ETHV, ethvUpdate } from '../generated/schema'
-import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
+import { Publish } from "./types/ethvindexv1/EthVIndexV1"
+import { Index, Update } from "./types/schema"
+import { BigInt, BigDecimal } from '@graphprotocol/graph-ts'
 
 const BigIntOne =  BigInt.fromI32(1)
 const BigIntZero =  BigInt.fromI32(0)
@@ -8,26 +8,25 @@ const ethvAddr = '0xe10bF493577B11f2161b7505b6a707186BFB6208'
 
 
 export function handlePublish(event: Publish): void {
-    let ethv = ETHV.load(ethvAddr)
-    if (ethv === null) {
-        ethv = new ETHV()
+    let ethv = Index.load(ethvAddr)
+    if (ethv == null) {
+        ethv = new Index(ethvAddr)
+        ethv.name = 'ETHV'
         ethv.latestUpdate = null
-        ethv.updates = []
         ethv.numUpdates = BigIntZero
-        return ethv
+        
+        ethv.save()
     }
 
-    let update = new ethvUpdate(event.params.id.toHex());
+    let update = new Update(event.transaction.hash.toHexString().toString());
 
     update.blocknumber = event.block.number;
     update.timestamp = event.block.timestamp;
-    update.ethv = event.params.ethv;
+    update.value = BigDecimal.fromString(event.params.ethv);
+    update.index = ethv.id;
     update.save();
 
     ethv.numUpdates = ethv.numUpdates + BigIntOne
-    let updates = ethv.updates
-    updates.push(update.id)
-    ethv.updates = updates
     ethv.latestUpdate = update.id
     ethv.save()
 }
